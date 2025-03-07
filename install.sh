7a8ba80c8d21c5693abd45293779ff08d47095cb
Bruno-MBl <bruno.maciejewski@alumnos.uneatlantico.es>
2025-03-07 23:05:39 +0100

    Mejoras en el script de instalado

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd ~/.dotfiles
-stow -t ~ .config/i3 .config/alacritty .config/picom
-fc-cache -fv  # Si incluyes fuentes
+#Instalar las fuentes
+./install-fonts.sh
+
+#Enlazar los paquetes al .config
+mkdir -p ~/.config/{i3,i3blocks,alacritty}
+cd ~/.dotfiles/.config/
+stow -t ~/.config/i3/ i3
+stow -t ~/.config/i3blocks/ i3blocks
+stow -t ~/.config/alacritty/ alacritty
+
